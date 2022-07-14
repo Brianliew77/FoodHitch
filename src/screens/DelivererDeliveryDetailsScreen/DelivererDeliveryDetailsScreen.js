23419a77d33d9b95328d1346f4d7f0456ec6f341
Brianliew77 <99940198+Brianliew77@users.noreply.github.com>
2022-07-14 22:23:56 +0800

    Update DelivererDeliveryDetailsScreen.js
    updating issue of timing going beyond 2359 and submission of delivery request with empty fields

--- src/screens/DelivererDeliveryDetailsScreen/DelivererDeliveryDetailsScreen.js
+++ src/screens/DelivererDeliveryDetailsScreen/DelivererDeliveryDetailsScreen.js
@@ -1,9 +1,9 @@
 import React, { useState } from 'react'
-import { Text, TextInput, TouchableOpacity, KeyboardAvoidingView, ScrollView } from 'react-native'
+import { Text, TextInput, TouchableOpacity, KeyboardAvoidingView, ScrollView, FlatList } from 'react-native'
 import styles from './styles';
 import { getAuth } from "firebase/auth";
 import { getFirestore, doc, updateDoc, setDoc } from "firebase/firestore"; 
-
+import DropDownPicker from 'react-native-dropdown-picker';
 
 export default function DelivererDeliveryDetails({route, navigation}) {
     const {deliveryPlace} = route.params;
@@ -11,6 +11,8 @@ export default function DelivererDeliveryDetails({route, navigation}) {
     const [ETA, onChangeETA] = useState(null)
     const [capacity, onChangeCapacity] = useState(null)
     const [deliveryStatus, onChangeDeliveryStatus] = useState('Order Pending')
+
+    //if ETA not 0000-2359, show alert and dont allow user to proceed
 
     const onPressConfirmDelivery = () => {
         const auth = getAuth();
@@ -28,7 +30,7 @@ export default function DelivererDeliveryDetails({route, navigation}) {
         const data3 = {
             deliveryStatus
         }
-        console.log("making new reqeeuest")
+        console.log("making new reqeuest")
         console.log(data2)
         setDoc( doc(db, 'Request', auth.currentUser.email), 
             data2
@@ -39,35 +41,31 @@ export default function DelivererDeliveryDetails({route, navigation}) {
             .catch((error) => {
                 alert(error)
             });
-        navigation.navigate('ShowingDeliveryDetails', {foodPlace: foodPlace, deliveryPlace: deliveryPlace, ETA: ETA, capacity: capacity})
-
-        // updateDoc(doc(db, 'Request', auth.currentUser.email), {
-        //     data3
-        //   }).then(() => {
-        //     // Profile updated!
-        //     console.log('test0')
-        //   }).catch((error) => {
-        //     // An error occurred
-        //     alert(error)
-        //   });           
-
-        // updateDoc(doc(db, 'user', auth.currentUser.email), {
-        //     data2
-        //   }).then(() => {
-        //     // Profile updated!
-        //     console.log('test1')
-        //     navigation.navigate('ShowingDeliveryDetails', {foodPlace: foodPlace, deliveryplace: deliveryplace, number: number, number1: number1})
-        //   }).catch((error) => {
-        //     // An error occurred
-        //     alert(error)
-        //   });
+        console.log(ETA.length)
+        console.log(capacity.length)
+        if (ETA.length != 4) {
+            alert("Please enter valid estimated delivery time!")
         }
-
+        else if (parseInt(ETA) > 2359) {
+            alert("Please enter valid estimated delivery time!")
+        }
+        //already restricted to 1 digit below hence this accounts for null value
+        else if (capacity.length != 1) {
+            alert("Please enter Order Capacity!")           
+        }
+        else if (parseInt(capacity) == 0) {
+            alert("Order capacity cannot be 0!")
+        }
+        else {
+            navigation.navigate('ShowingDeliveryDetails', {foodPlace: foodPlace, deliveryPlace: deliveryPlace, ETA: ETA, capacity: capacity})
+        }
+        }
 
     return (
 
         <KeyboardAvoidingView style={styles.container}>
         <ScrollView style={styles.scrollview}>
+
             <Text style={styles.text4}>You are buying from: </Text>
             <Text style={styles.text4italics}>{foodPlace}</Text>
 
@@ -88,6 +86,7 @@ export default function DelivererDeliveryDetails({route, navigation}) {
                 keyboardType="numeric"
                 maxLength={4}
             />
+
             <Text style={styles.text4}>Order Capacity:</Text>
 
             <TextInput
@@ -96,7 +95,7 @@ export default function DelivererDeliveryDetails({route, navigation}) {
                 value={capacity}
                 placeholder="Order Capacity"
                 keyboardType="numeric"
-                maxLength={2}
+                maxLength={1}
             />
 
             <TouchableOpacity
@@ -104,6 +103,7 @@ export default function DelivererDeliveryDetails({route, navigation}) {
                     onPress={() => onPressConfirmDelivery()}>
                     <Text style={styles.buttonTitle}>Confirm Delivery</Text>
                 </TouchableOpacity>
+        
         </ScrollView>
         </KeyboardAvoidingView>
 
