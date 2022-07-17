26b634aef3d32d8994844c3481814b2e591fe49b
Dxseah <64683658+Dxseah@users.noreply.github.com>
2022-07-18 01:45:14 +0800

    Included flatlist to display orders
    Request screen edit

--- src/screens/DelivererCurrentOrdersScreen/DelivererCurrentOrdersScreen.js
+++ src/screens/DelivererCurrentOrdersScreen/DelivererCurrentOrdersScreen.js
@@ -1,6 +1,6 @@
 
 import React, { useEffect, useState } from 'react'
-import { Text, SafeAreaView, TouchableOpacity } from 'react-native'
+import { Text, SafeAreaView, TouchableOpacity, FlatList, View} from 'react-native'
 import { getAuth} from "firebase/auth";
 import styles from './styles';
 import { getFirestore, doc, getDoc, updateDoc, getDocs, onSnapshot,  collection, query, where } from "firebase/firestore";
@@ -8,11 +8,40 @@ import { ScrollView } from 'react-native-gesture-handler';
 
 export default function DelivererCurrentOrdersScreen({route, navigation}) {
 
-    const [ordEmail, onChangeOrdEmail] = useState([])
-    const [totPrice, onChangeTotPrice] = useState([])
-    const [foodie1, onChangeFoodie1] = useState([])
-    const [foodie2, onChangeFoodie2] = useState([])
 
+    const [orderData, onChangeOrderData] = useState([])
+    useEffect(()=>{
+        const db = getFirestore();
+        const auth = getAuth();
+
+        const reqRef = collection(db, "Order");
+        const q = query(reqRef, where("delivererEmail", "==", auth.currentUser.email));
+        const querySnapshot = getDocs(q).then( querySnapshot =>
+        querySnapshot.forEach((doc) => {
+          onChangeOrderData(prevState=>[...prevState, 
+            [doc.data().ordererEmail,doc.data().totalPrice,
+              doc.data().food.food1,doc.data().food.food2]])
+        })
+        )
+    },[])
+    const Item = ({ item}) => (
+      <View>
+          <Text style={styles.text4}>Orderer Email: {item[0]}</Text>
+
+          <Text style={styles.text4}>Food Item 1: {item[2]}</Text>
+          <Text style={styles.text4}>Food Item 2: {item[3]}</Text>
+      
+          <Text style={styles.text4}>Total Price Of Order: {item[1]}</Text>
+      </View>
+      );
+    const renderItem = ({ item }) => {
+
+        return (
+        <Item
+            item={item}
+        />
+        );
+    };
     const onPressisPending = () => {
         const auth = getAuth();
         const db = getFirestore();
@@ -73,41 +102,11 @@ export default function DelivererCurrentOrdersScreen({route, navigation}) {
             }
 
     //need account for the case where there isnt any -> show an alert
-    const onClickCheckEmail = () => {
-        const db = getFirestore();
-        const auth = getAuth();
 
-        const reqRef = collection(db, "Order");
-        const q = query(reqRef, where("delivererEmail", "==", auth.currentUser.email));
-        const querySnapshot = getDocs(q).then( querySnapshot =>
-        querySnapshot.forEach((doc) => {
-            console.log("Printing Orderer Email")
-            //Printing Orderer Email
-            onChangeOrdEmail(()=> {return [doc.data().ordererEmail]})
-
-            console.log("Printing Food Item 1")
-            //Printing Food Item 1
-            onChangeFoodie1(()=> {return [doc.data().food.food1]})
-
-            console.log("Printing Food Item 2")
-            //Printing Food Item 2
-            onChangeFoodie2(()=> {return [doc.data().food.food2]})
-
-            console.log("Printing Total Price")
-            //Printing Total Price
-            onChangeTotPrice(()=> {return [doc.data().totalPrice]})
-
-
-
-        }
-        ))
-    }  
- 
+    console.log(orderData)
 
     return (
     <SafeAreaView style={styles.container}>
-    <ScrollView style={styles.scrollview}>
-    
     <Text style={styles.text2}>{'\n'}</Text>
 
     <TouchableOpacity
@@ -119,13 +118,21 @@ export default function DelivererCurrentOrdersScreen({route, navigation}) {
     <Text style={styles.text}>Your Current Orders Are:</Text>
 
     <Text style={styles.text2}>{'\n'}</Text>
-
+    <FlatList 
+                data={orderData}
+                renderItem={renderItem}
+                keyExtractor={(item) => item[0]}
+                // onRefresh={onRefreshPress}
+                // refreshing={isFetching}
+                ListEmptyComponent={<Text style={styles.noRequest}>No requests found.</Text>}
+            />
+{/* 
     <Text style={styles.text4}>Orderer Email: {ordEmail}</Text>
 
     <Text style={styles.text4}>Food Item 1: {foodie1}</Text>
     <Text style={styles.text4}>Food Item 2: {foodie2}</Text>
 
-    <Text style={styles.text4}>Total Price Of Order: {totPrice}</Text>
+    <Text style={styles.text4}>Total Price Of Order: {totPrice}</Text> */}
 
     <Text style={styles.text4}>Update Orders Status:</Text>
             
@@ -146,7 +153,7 @@ export default function DelivererCurrentOrdersScreen({route, navigation}) {
                     onPress={() => onPressisDelivered()}>
                     <Text style={styles.buttonTitle}>Order Delivered</Text>
             </TouchableOpacity>
-    </ScrollView>
+
     </SafeAreaView>
     )
 }
