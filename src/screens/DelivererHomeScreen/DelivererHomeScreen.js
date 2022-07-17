8b7551a9e3b2f8d39728cdd03e72003fa07ca75e
Dxseah <64683658+Dxseah@users.noreply.github.com>
2022-07-18 01:52:22 +0800

    add profile screen navigation
    added profile feature

--- src/screens/DelivererHomeScreen/DelivererHomeScreen.js
+++ src/screens/DelivererHomeScreen/DelivererHomeScreen.js
@@ -1,19 +1,38 @@
-import React from 'react'
-import { Text, TouchableOpacity, SafeAreaView, ImageBackground } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import { Image,Text, TouchableOpacity, SafeAreaView, ImageBackground, View } from 'react-native'
 import styles from './styles';
-
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { getFirestore, doc, getDoc } from "firebase/firestore";
 
 export default function DelivererHomeScreen({route, navigation}) {
     const onMakeNewReq = () => {navigation.navigate("WhereToBuyFood")}
     const onCheckCurrentReq = () => {navigation.navigate("ShowingRequest")}
-
- 
-
+    const [userName,setUserName] = useState("")
+    const auth = getAuth()
+    const db = getFirestore()
+    useEffect(()=>{
+        const docRefName = doc(db,"user", auth.currentUser.email)
+        const docSnapName = getDoc(docRefName)
+        .then(docSnap=>{
+            if (docSnap.exists()) {
+                setUserName(docSnap.data().data.fullName)
+            } else {
+                alert("No user logged in")
+                return
+        }
+        })
+    },[])
     return (
         <SafeAreaView style={styles.container}>
 
             <Text style={styles.text2}>{'\n'}</Text>
-
+            <View style={styles.profileWrap}>
+            <Text style={styles.welcomeText1}> Welcome</Text>
+            <Text style={styles.welcomeText2}> {userName}!</Text>
+            <TouchableOpacity onPress={()=>{navigation.navigate("Profile")}}> 
+                <Image style={styles.profileImage} source={require('../../../assets/profile.png')}/>
+            </TouchableOpacity>
+        </View>
             <TouchableOpacity 
                     onPress={() => onMakeNewReq()}>
             <ImageBackground style = {styles.button2} source={require('../../../assets/foodstall.jpg')}>
